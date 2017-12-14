93ef85b184054e3c26d7d3d48b5b7357b0fbe195
Kevin Smith <kevin@muny.us>
2017-12-14 00:03:23 -0500

    initial commit

---
+++ Pictural Line Segments/Form1.Designer.cs
@@ -0,0 +1,163 @@
+﻿namespace Pictural_Line_Segments
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listBox1.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawVariable;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 27;
+            this.listBox1.Location = new System.Drawing.Point(1273, 26);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(159, 654);
+            this.listBox1.TabIndex = 0;
+            this.listBox1.DrawItem += new System.Windows.Forms.DrawItemEventHandler(this.listBox1_DrawItem);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(1299, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Equations";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(18, 5);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(73, 17);
+            this.checkBox1.TabIndex = 3;
+            this.checkBox1.Text = "Draw Grid";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox2.Location = new System.Drawing.Point(113, 5);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(83, 17);
+            this.checkBox2.TabIndex = 4;
+            this.checkBox2.Text = "Draw Image";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            this.checkBox2.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Checked = true;
+            this.checkBox3.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox3.Location = new System.Drawing.Point(202, 5);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(124, 17);
+            this.checkBox3.TabIndex = 5;
+            this.checkBox3.Text = "Draw Line Segments";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            this.checkBox3.CheckedChanged += new System.EventHandler(this.checkBox3_CheckedChanged);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left)));
+            this.pictureBox1.BackgroundImage = global::Pictural_Line_Segments.Properties.Resources.iss_with_grid;
+            this.pictureBox1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.pictureBox1.ImageLocation = "";
+            this.pictureBox1.Location = new System.Drawing.Point(29, 26);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1231, 629);
+            this.pictureBox1.TabIndex = 2;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(17, 646);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1432, 697);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listBox1);
+            this.DoubleBuffered = true;
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Pictural Line Segments";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseMove);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ Pictural Line Segments/Form1.cs
@@ -0,0 +1,325 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Pictural_Line_Segments
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        Pen pen = new Pen(new SolidBrush(Color.Black), 1);
+        Pen hover_pen = new Pen(new SolidBrush(Color.Red), 2);
+        Pen registered_pen = new Pen(new SolidBrush(Color.Blue), 2);
+
+        Graphics g;
+
+        Point cursor_rel = new Point();
+
+        bool left_click = false;
+        bool right_click = false;
+
+        List<LineSegment> LineSegments = new List<LineSegment>();
+
+        public Point CurrentFirstPoint = new Point(-1,-1);
+
+        Point LastHoveredPoint = new Point(-1, -1);
+
+        int delt = 5;
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            g = e.Graphics;
+
+            g.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+            g.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBilinear;
+            g.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+
+            for (int h = 0; h < pictureBox1.Height; h += 10)
+            {
+                for (int w = 0; w < pictureBox1.Width; w += 10)
+                {
+                    if (cursor_rel.X > w - delt && cursor_rel.X < w + delt && cursor_rel.Y > h - delt && cursor_rel.Y < h + delt)
+                    {
+                        LastHoveredPoint = new Point(w, h);
+                        if (left_click)
+                        {
+                            if (CurrentFirstPoint.X == -1 && CurrentFirstPoint.Y == -1)
+                            {
+                                CurrentFirstPoint = new Point(w, h);
+                            }
+                            else
+                            {
+                                LineSegments.Add(new LineSegment(CurrentFirstPoint, new Point(w, h)));
+                                CurrentFirstPoint = new Point(-1,-1);
+
+                                UpdateListBox();
+                            }
+                            left_click = false;
+                        }
+                        else if (right_click)
+                        {
+                            List<LineSegment> where1 = LineSegments.Where(value => (value.FirstPoint.X == LastHoveredPoint.X && value.FirstPoint.Y == LastHoveredPoint.Y)).ToList();
+                            List<LineSegment> where2 = LineSegments.Where(value => (value.SecondPoint.X == LastHoveredPoint.X && value.SecondPoint.Y == LastHoveredPoint.Y)).ToList();
+
+                            try
+                            {
+                                    // clicked on outer point of complete line segment
+                                    if (where1.Count() > 0)
+                                    {
+                                        CurrentFirstPoint = where1[0].SecondPoint;
+                                        LineSegments.Remove(where1[0]);
+                                    }
+                                    else if (where2.Count() > 0)
+                                    {
+                                        CurrentFirstPoint = where2[0].FirstPoint;
+                                        LineSegments.Remove(where2[0]);
+                                    }
+                                // right clicked the anchor of the currently selected line
+                                else if (CurrentFirstPoint.X != -1 && CurrentFirstPoint.Y != -1)
+                                {
+                                    CurrentFirstPoint = new Point(-1, -1);
+                                }
+                            }
+                            catch (Exception ex)
+                            {
+                                throw ex;
+                            }
+
+                            UpdateListBox();
+
+                            right_click = false;
+                        }
+                        /*g.DrawLine(hover_pen, new Point(w, h - 3), new Point(w, h + 3));
+                        g.DrawLine(hover_pen, new Point(w - 3, h), new Point(w + 3, h));*/
+
+                        if (CurrentFirstPoint.X == -1 && CurrentFirstPoint.Y == -1)
+                        {
+                            g.DrawRectangle(hover_pen, new Rectangle(new Point(LastHoveredPoint.X - 2, LastHoveredPoint.Y - 2), new Size(4,4)));
+                        }
+                    }
+
+                    /*else if (checkBox1.Checked)
+                    {
+                        g.DrawLine(pen, new Point(w, h - 3), new Point(w, h + 3));
+                        g.DrawLine(pen, new Point(w - 3, h), new Point(w + 3, h));
+                    }*/
+
+                }
+            }
+
+            if (CurrentFirstPoint.X != -1 && CurrentFirstPoint.Y != -1)
+            {
+                g.DrawRectangle(registered_pen, new Rectangle(new Point(CurrentFirstPoint.X - 2, CurrentFirstPoint.Y - 2), new Size(4, 4)));
+                g.DrawLine(new Pen(new SolidBrush(Color.Green), 2), CurrentFirstPoint, LastHoveredPoint);
+                g.DrawRectangle(hover_pen, new Rectangle(new Point(LastHoveredPoint.X - 2, LastHoveredPoint.Y - 2), new Size(4, 4)));
+                //g.DrawLine(new Pen(new SolidBrush(Color.Green), 2), CurrentFirstPoint, cursor_rel);
+            }
+
+            if (checkBox3.Checked)
+            {
+
+                foreach (LineSegment seg in LineSegments)
+                {
+                    g.DrawRectangle(registered_pen, new Rectangle(new Point(seg.FirstPoint.X - 2, seg.FirstPoint.Y - 2), new Size(4,4)));
+                    g.DrawRectangle(registered_pen, new Rectangle(new Point(seg.SecondPoint.X - 2, seg.SecondPoint.Y - 2), new Size(4,4)));
+                    g.DrawLine(new Pen(new SolidBrush(Color.Blue), 2), seg.FirstPoint, seg.SecondPoint);
+                }
+            }
+        }
+
+        private void UpdateListBox()
+        {
+            listBox1.Items.Clear();
+            foreach (LineSegment seg in LineSegments)
+            {
+                string equation = "";
+
+                // point 1
+                double x1 = seg.FirstPoint.X / 10;
+                double y1 = 63 - (seg.FirstPoint.Y / 10);
+
+                // point 2
+                double x2 = seg.SecondPoint.X / 10;
+                double y2 = 63 - seg.SecondPoint.Y / 10;
+
+                double numerator = y1 - y2;
+                double denominator = x1 - x2;
+
+                // vertical line
+                if (denominator == 0)
+                {
+                    equation = "x = " + seg.SecondPoint.X / 10;
+
+                    double smallest = 0;
+                    double largest = 0;
+
+                    if (y1 < y2)
+                    {
+                        smallest = y1;
+                        largest = y2;
+                    }
+                    else
+                    {
+                        smallest = y2;
+                        largest = y1;
+                    }
+
+                    equation += "\n" + smallest + " <= y <=" + largest;
+                }
+                // horizontal or angled line
+                else
+                {
+                    bool anghor = false;
+
+                    equation = "y = ";
+
+                    // angled line
+                    if ((numerator / denominator) != 0)
+                    {
+                        anghor = true;
+                        equation += Math.Round((double)(numerator / denominator), 3) + "x";
+                    }
+                    // horizontal line
+                    else
+                    {
+                        anghor = false;
+                    }
+
+                    if ((63 - (seg.SecondPoint.Y / 10)) != 0)
+                    {
+                        if (equation != "y = ")
+                            equation += " + ";
+
+                        equation += 63 - (seg.SecondPoint.Y / 10);
+                    }
+
+                    // angled
+                    if (anghor)
+                    {
+                        //equation += " (angled)";
+                    }
+                    // horizontal
+                    else
+                    {
+                        //equation += " (horizontal)";
+                    }
+
+                    double smallest = 0;
+                    double largest = 0;
+
+                    if (x1 < x2)
+                    {
+                        smallest = x1;
+                        largest = x2;
+                    }
+                    else
+                    {
+                        smallest = x2;
+                        largest = x1;
+                    }
+
+                    equation += "\n" + smallest + " <= x <=" + largest;
+                }
+
+
+                listBox1.Items.Add(equation);
+            }
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)
+        {
+            cursor_rel = e.Location;
+            pictureBox1.Refresh();   
+        }
+
+        private void Form1_MouseMove(object sender, MouseEventArgs e)
+        {
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (e.Button == System.Windows.Forms.MouseButtons.Left)
+                left_click = true;
+            else if (e.Button == System.Windows.Forms.MouseButtons.Right)
+                right_click = true;
+
+            pictureBox1.Refresh();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox2.Checked)
+            {
+                if (checkBox1.Checked)
+                    pictureBox1.BackgroundImage = Properties.Resources.iss_with_grid;
+                else
+                    pictureBox1.BackgroundImage = Properties.Resources._693259main_jsc2012e219094_big1;
+            }
+        }
+
+        private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+            if (!checkBox2.Checked)
+            {
+                pictureBox1.BackgroundImage = null;
+            }
+            else
+            {
+                if (checkBox1.Checked)
+                    pictureBox1.BackgroundImage = Properties.Resources.iss_with_grid;
+                else
+                    pictureBox1.BackgroundImage = Properties.Resources._693259main_jsc2012e219094_big1;
+            }
+        }
+
+        private void checkBox3_CheckedChanged(object sender, EventArgs e)
+        {
+            pictureBox1.Refresh();
+        }
+
+        private void listBox1_DrawItem(object sender, DrawItemEventArgs e)
+        {
+            try
+            {
+                e.DrawBackground();
+                e.Graphics.CompositingMode = System.Drawing.Drawing2D.CompositingMode.SourceCopy;
+                e.Graphics.CompositingQuality = System.Drawing.Drawing2D.CompositingQuality.HighQuality;
+                e.Graphics.InterpolationMode = System.Drawing.Drawing2D.InterpolationMode.HighQualityBicubic;
+                e.Graphics.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.HighQuality;
+                e.Graphics.TextRenderingHint = System.Drawing.Text.TextRenderingHint.AntiAliasGridFit;
+                e.Graphics.DrawString(listBox1.Items[e.Index].ToString(), e.Font, new SolidBrush(e.ForeColor), e.Bounds);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+
+    public class LineSegment
+    {
+        public Point FirstPoint;
+        public Point SecondPoint;
+        public LineSegment(Point FirstPoint, Point SecondPoint)
+        {
+            this.FirstPoint = FirstPoint;
+            this.SecondPoint = SecondPoint;
+        }
+    }
+}
